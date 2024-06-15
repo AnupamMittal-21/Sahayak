6ec04cd25f20b8265e4aa2f153f972d7476a55e3
AnupamMittal-21 <anupammittal1234@gmail.com>
2024-06-16 00:23:37 +0530

    added Additional files.

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ if __name__ == "__main__":
     print(f"Region is : {os.environ.get('REGION')}")
     port = int(os.environ.get("PORT", 8000))
     print(f"Starting server on port {port}")
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run('main:app', host="0.0.0.0", port=port, reload= True)
